484dcf14c3b1be4fc3cdcdd7d73ff75d65570297
razzlepdx <rwilson.or@gmail.com>
2017-06-19 13:24:29 -0700

    add quotes and twitter functionality

--- RQM/script.js
+++ RQM/script.js
@@ -6,22 +6,55 @@ var quotes = [
   {quote: "YER A WIZARD, HARRY",
   source: "hagrid"},
   {quote: "avada kedavra",
-  source: "HE WHO MUST NOT BE NAMED"}
+  source: "HE WHO MUST NOT BE NAMED"},
+  {quote: "Me fail english, thats unpossible!",
+  source: "Ralph Riggum"},
+  {quote: "I typed your symptom into the thing up here, and it says you could have network connectivity problems. ",
+  source: "Andy Dywer"},
+  {quote: "Be Prepared!",
+  source: "Scar"},
+  {quote: "He looka like a man",
+  source: "Ms Swan"},
+  {quote: "Cool Prius!",
+  source: "Noone ever"},
+  {quote: "I have a dream today",
+  source: "Dr. Martin Luther King Jr"},
+  {quote: "Hello",
+  source: "Benjamin Franklin (Probably)"},
+  {quote: "Nope",
+  source: "Chuck Testa"},
+  {quote: "Ain't nobody got time for that",
+  source: "Sweet Brown"},
+  {quote: "I've got greens, beans, potatoes, tomatoes, lambs, rams, hogs, dogs, chickens, turkeys... You name it!",
+  source: "Shirley Caesar"},
+  {quote: "Who is you?",
+  source: "Rachel EB Wilson"},
+  {quote: "ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+  source: "Whispers"}
 ];
 
 // page element selectors
 // =============================== 
 var button = document.querySelector('button');
-var displayArea = document.querySelector('#displayit p');
+var displayArea = document.querySelectorAll('#displayit .fa');
+var quoteDisplay = document.getElementById('quoteDisplay');
+var authorDisplay = document.getElementById('authorDisplay');
 var tweetButton = document.querySelector('#shareit i');
 
 //event listeners
 //===============================
+//new quote button
 button.addEventListener('click', displayNew);
-button.addEventListener('mouseover', hover);
-button.addEventListener('mouseout', hover);
+button.addEventListener('mousedown', clicked);
+button.addEventListener('mouseup', clicked);
+button.addEventListener('mouseover', darken);
+button.addEventListener('mouseout', darken);
+//tweet button
+tweetButton.addEventListener('click', tweetQuote);
 tweetButton.addEventListener('mouseover', sizeup);
 tweetButton.addEventListener('mouseout', sizeup);
+tweetButton.addEventListener('mousedown', sizeup);
+tweetButton.addEventListener('mouseup', sizeup);
 
 //functions
 //================================
@@ -29,8 +62,8 @@ tweetButton.addEventListener('mouseout', sizeup);
 //main button-click response: display new quote
 function displayNew(){
 	var ind = newIndex();
-	var quote = newQuote(ind);
-	return displayArea.textContent = quote;
+	quoteDisplay.textContent = quotes[ind].quote;
+  authorDisplay.textContent = "- " + quotes[ind].source;
 }
 
 //pick random index for new quotes
@@ -38,15 +71,21 @@ function newIndex () {
   return Math.floor(Math.random() * quotes.length);
 }
 
-//use random index and format/prep quote to add to page
-function newQuote(num) {
-  var index = newIndex();
-  return "'" + quotes[index].quote + "' - " + quotes[index].source;
+//tweet current quote
+function tweetQuote() { 
+  var url = "https://twitter.com/intent/tweet";
+  var text= "\"" + document.getElementById("quoteDisplay").textContent + "\" " + document.getElementById("authorDisplay").textContent;
+  var hashtags = "chingus,fCC";
+
+  window.open(url + "?text=" + text + ";hashtags=" + hashtags, "width=500,heigth=300");
 }
 
-//button hover effects
-function hover() {
-	this.classList.toggle("hover");
+//button click and hover effects
+function darken() {
+  this.classList.toggle("darken");
+}
+function clicked() {
+	this.classList.toggle("clicked");
 }
 function sizeup() {
 	this.classList.toggle('fa-3x');
