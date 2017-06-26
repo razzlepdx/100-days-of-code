e866876fcb62a70b6fc544ba0d508907fbe6638e
razzlepdx <rwilson.or@gmail.com>
2017-06-26 09:38:46 -0700

    initial commit

---
+++ LocalWeather/script.js
@@ -0,0 +1,9 @@
+var parsedData;
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  parsedData = JSON.parse(body);
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', parsedData); // Print the HTML for the Google homepage.
+});
